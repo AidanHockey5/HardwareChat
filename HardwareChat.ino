949c4ca791e95dedc966554a9d4c0d5deb9f74e7
Aidan Lawrence <aidan@aidanlawrence.com>
2018-12-18 19:05:21 -0800

    Basic chat works!

--- HardwareChat.ino
+++ HardwareChat.ino
@@ -1,9 +1,108 @@
-void setup() {
-  // put your setup code here, to run once:
+//2018 Aidan Lawrence, www.AidanLawrence.com
+//A simple chat service that you can access via your internet-enabled Arduino hardware!
+//CONNECTIONS ARE NOT SECURE AND ALL DATA IS PUBLIC! DO NOT SHARE ANY SENSITIVE INFORMATION.
+//IP Addresses are not shared to any other client.
+//Yes, of course you can modify this script and have some fun, just be cool about it when connecting to the server, please!
+//Have fun and be nice!
 
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!ADD YOUR WIFI CREDENTIALS!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#define AP_SSID "ADD_YOUR_WIFI_SSID_HERE"
+#define AP_PASS "ADD_YOUR_WIFI_PASSWORD_HERE"
+
+#define MQTT_SERVER "mqtt.aidanlawrence.com"
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include "MQTT.h"
+
+WiFiClient net;
+MQTTClient client(256);
+String userName = "";
+
+void setup() 
+{
+  Serial.begin(115200);
+  while(!Serial){delay(1);}
+  SetCredentials();
+  ConnectToWiFi();
+  client.begin(MQTT_SERVER, net);
+  client.onMessage(Incomming);
+  ConnectToBroker();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void SetCredentials()
+{
+  Serial.println("Please enter your username [16 chars max]: ");
+  while(!Serial.available()){delay(1);}
+  while(Serial.available())
+  {
+    userName = Serial.readString(); 
+    delay(1);
+  }
+  if(userName.length() > 16)
+    userName.remove(16);
+  //if(userName.endsWith(char(0x0D)))
+  userName.trim();
+  Serial.print("Your username will be: "); Serial.println(userName);
+}
 
+bool ConnectToWiFi()
+{
+  Serial.print("Connecting to AP: "); Serial.println(AP_SSID);
+  WiFi.begin(AP_SSID, AP_PASS);
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Connected to WiFi AP");
+  return true;
+}
+
+bool ConnectToBroker()
+{
+  Serial.print("Attempting to connect to: "); Serial.println(MQTT_SERVER);
+  while(!client.connect(&userName[0u], &userName[0u]))
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.println("Connected! Have fun and be nice!");
+  client.subscribe("/chat"); //Subscribe to the main chat
+  client.subscribe("/"+userName); //Subscribe to private messages (NOTE, THESE ARE NOT SECURE AT ALL! Anyone can see these PMs if they try!)
+  
+  client.publish("/chat", userName+" has joined the chat.", false, 2);
+  return true;
+}
+
+void Incomming(String &topic, String &payload)
+{
+  Serial.println(payload);
+}
+
+String sendBuf = "";
+void loop() 
+{
+  client.loop();
+  #if defined(ESP8266)
+  delay(10);
+  #endif
+  if(!client.connected())
+  {
+    ConnectToBroker();
+  }
+  while(Serial.available())
+  {
+    sendBuf = Serial.readString();
+    yield();
+  }
+  if(sendBuf != "")
+  {
+    client.publish("/chat", "["+userName+"] "+sendBuf, false, 2);
+    sendBuf = "";
+  }
 }
